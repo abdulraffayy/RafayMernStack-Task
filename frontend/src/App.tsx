762dccce5f9b85d9a65e856d5fb18bd819cbffea
Abdul Rafay <rajpootrafay96@gmail.com>
2025-10-09 22:10:03 +0500

    Created a project of task frontend and backend with react typescript and shdecn ui an tailwind css and shadecn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "./components/ui/button"
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-3xl font-bold underline bg-red-500 text-white p-4 rounded-md m-4'>Hello World</h1>
+     <Button variant="outline">Button</Button>
+    </>
+  )
+}
+
+export default App
